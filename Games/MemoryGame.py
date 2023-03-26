283e43848b52d21c220cb351765cf1529470d29a
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 <eldadtzabari@gmail.com>
2023-03-26 15:58:56 +0300

    made some changes so the code its more readable and created folders

--- Games/MemoryGame.py
+++ Games/MemoryGame.py
@@ -2,13 +2,13 @@ import random
 import time
 import Live
 import os
-
+from Utils import Screen_cleaner
 def generate_sequence(difficulty):
     secret_sequence = [random.randint(1, 101) for i in range(int(difficulty))]
     return secret_sequence
 
 def get_list_from_user():
-    user_input = input("Please enter your numbers")
+    user_input = input("Please enter your numbers: ")
     user_list = [int(no) for no in user_input.split()]
     return user_list
 
@@ -25,6 +25,7 @@ def play(difficulty):
 
     secret_sequence = generate_sequence(difficulty)
     print(secret_sequence)
+    Screen_cleaner()
     result = is_list_equal(secret_sequence, get_list_from_user())
 
 
--- Utils.py
+++ Utils.py
@@ -1,15 +1,17 @@
 import os
 import time
 
-SCORES_FILE_NAME = 'Scores.txt'
+SCORES_FILE_NAME = 'Scores/Scores.txt'
 
 BAD_RETURN_CODE = "Error code 200"
 
 def Screen_cleaner():
-
-
     time.sleep(0.7)
-    os.system('cls')
-Screen_cleaner()
+    if os.name == 'nt':  # Check if the OS is Windows
+        os.system('cls')  # Clear the screen using "cls" command
+    else:  # if the if block its not returning True then it will use it for linux
+        os.system('clear')  # Clear the screen using "clear" command
+
+
 
 
