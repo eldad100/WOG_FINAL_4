5216312107a9f041703a8c8cd251b70e8a305a01
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 <eldadtzabari@gmail.com>
2023-05-09 15:37:40 +0300

    a

---
+++ Scores/Score.py
@@ -0,0 +1,33 @@
+# from Utils import SCORES_FILE_NAME
+# import os
+#
+# def add_score(difficulty):
+#     POINTS_OF_WINNING = (difficulty * 3) + 5
+#     if os.path.exists(SCORES_FILE_NAME):
+#         with open(SCORES_FILE_NAME, 'r') as current_score:
+#             score = current_score.read()
+#         with open(SCORES_FILE_NAME, 'w') as write_score:
+#             scores = write_score.write(str(POINTS_OF_WINNING))
+#
+#
+#     new_score = score + str(POINTS_OF_WINNING)
+#     with open(SCORES_FILE_NAME, "w") as scor:
+#         scor.write(new_score)
+#     return new_score
+import os  # import the "os" module
+from Utils import SCORES_FILE_NAME  # import the "SCORES_FILE_NAME" constant from the "Utils" module
+
+# define a function named "add_score" that takes one argument: lvl_sel
+def add_score(lvl_sel):
+    POINTS_OF_WINNING = (lvl_sel * 3) + 5  # calculate the points of winning based on the difficulty level
+    if os.path.exists(SCORES_FILE_NAME):  # check if the score file exists
+        with open(SCORES_FILE_NAME, 'r') as scores:  # open the score file for reading
+            scores_list = scores.readlines()  # read all lines in the file as a list
+        scores_list = [int(x.strip()) for x in scores_list]  # convert all items in the list to integers
+        current_score = sum(scores_list)  # sum the values in the list to get the current score
+    else:
+        current_score = 0  # if the score file does not exist, set the current score to 0
+    update_score = current_score + POINTS_OF_WINNING  # calculate the updated score
+    with open(SCORES_FILE_NAME, 'w') as scores:  # open the score file for writing
+        scores.write(str(update_score))  # write the updated score to the score file as a string
+    return update_score  # return the updated score
